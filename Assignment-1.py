6b2759bb104511bdcb53dfcd505f064ef440196f
jayantreddy00 <jayantreddy00@gmail.com>
2025-10-04 15:38:42 +0530

    Added Task-1 & Task-2

---
+++ Assignment-1.py
@@ -0,0 +1,26 @@
+# Task 1: Perform Basic Operations
+
+a=20
+b=10
+
+# Addition
+print(a+b)
+
+# Subtraction
+print(a-b)
+
+# Multiplication
+print(a*b)
+
+# Division
+print(a/b)
+
+# Task 2: Create a Personalized Greeting
+
+#1.
+firstname = "Jayant"
+lastname = " Reddy"
+print(firstname + lastname)
+
+#2.
+print("Hello" + " Jayant" + " Reddy!" + " Welcome to Python" + " programming," + " All the Best from Tute dude Team.")
